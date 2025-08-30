bd7954200c19818ee97904f738fd3903f3063e70
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-08-30 19:44:17 +0330

    Initiate blazor hybrid.

---
+++ src/RealtimeSample.BlazorHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RealtimeSample.BlazorHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
